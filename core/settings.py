b18211e20b72eb16f8c2d19970b794d3ab7a6093
Alex Jr <alexjunior2018@gmail.com>
2023-03-24 11:17:48 -0300

    changing database to mysql

--- core/settings.py
+++ core/settings.py
@@ -65,10 +65,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'residuo',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
