aaf6f0aa4f2ae19a0ba7ceb8c1f219ed093f9479
Zaysu <80471248+Zaysu@users.noreply.github.com>
2023-05-30 00:32:34 -0300

    Listar
    Listando as informações dos residuos

--- residuo/urls.py
+++ residuo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from residuo import views
+from residuo import views, view_listar_dados
 
 urlpatterns = [
     path('home/', views.index, name='index'),
@@ -9,4 +9,9 @@ urlpatterns = [
     path('Cadastro_residuo/', views.form_residuo, name='form_residuo'),
     path('Cadastro_categoria/', views.form_categoria, name='form_categoria'),
     path('Cadastro_solicitacao/', views.form_solicitacao, name='form_solicitacao'),
+    path('listar_pessoa/', view_listar_dados.listar_pessoa, name='listar_pessoa'),
+    path('listar_endereco/', view_listar_dados.listar_endereco, name='listar_endereco'),
+    path('listar_residuo/', view_listar_dados.listar_residuo, name='listar_residuo'),
+    path('listar_solicitacao/', view_listar_dados.listar_solicitacao, name='listar_solicitacao'),
+    path('listar_categoria/', view_listar_dados.listar_categoria, name='listar_categoria'),
 ]
---
+++ residuo/view_listar_dados.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import Pessoa, Endereco, Residuo, Solicitacao, Categoria
+
+def listar_pessoa(request):
+    pessoas = Pessoa.objects.all()
+    context = {
+        'pessoas' : pessoas
+    }
+    return render(request, 'listar_index.html', context)
+
+def listar_endereco(request):
+    enderecos = Endereco.objects.all()
+    context = {
+        'enderecos' : enderecos
+    }
+    return render(request, 'listar_endereco.html', context)
+
+def listar_residuo(request):
+    residuos = Residuo.objects.all()
+    context = {
+        'residuos' : residuos
+    }
+    return render(request, 'listar_residuo.html', context)
+
+def listar_solicitacao(request):
+    solicitacoes = Solicitacao.objects.all()
+    context = {
+        'solicitacoes' : solicitacoes
+    }
+    return render(request, 'listar_solicitacao.html', context)
+
+def listar_categoria(request):
+    categorias = Categoria.objects.all()
+    context = {
+        'categorias' : categorias
+    }
+    return render(request, 'listar_categoria.html', context)
